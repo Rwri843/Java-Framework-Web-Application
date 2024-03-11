f17aa9b6f70d0593ebb011344524b2a177e4e84b
Rwri843 <rwri326@wgu.edu>
2024-03-11 11:57:39 -0500

    AboutController.java bugs fixed.

--- src/main/java/com/example/demo/controllers/AboutController.java
+++ src/main/java/com/example/demo/controllers/AboutController.java
@@ -1,10 +1,16 @@
 package com.example.demo.controllers;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
+@RequestMapping("/about")
 public class AboutController {
-    @GetMapping("/about")
-    public String about() {
+
+    //creates endpoint to display About page
+    @GetMapping
+    public String showAbout() {
         return "about";
     }
 }
